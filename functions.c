42f3ce0b8bf911757a8c946941211d88c674f4d9
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-17 15:10:53 +0300

    Add _putchar and pString with NULL string handling

--- functions.c
+++ functions.c
@@ -1,1 +1,35 @@
 #include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * pString - prints a string
+ * @string: string to print
+ *
+ * Return: number of characters printed
+ */
+int pString(char *string)
+{
+	int count = 0;
+
+	if (string == NULL)
+		string = "(null)";
+
+	while (*string)
+	{
+		count += _putchar(*string);
+		string++;
+	}
+
+	return (count);
+}
