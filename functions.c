7aabdf34cde0369775929646b47145812a268a4b
Lama <lama72316@gmail.com>
2026-04-17 12:42:57 +0000

    Add integer printing function

--- functions.c
+++ functions.c
@@ -33,3 +33,32 @@ int pString(char *string)
 
 	return (count);
 }
+
+
+/**
+ 
+print_num - prints an integer
+@n: number to print*
+Return: number of characters printed*/
+int print_num(int n)
+{
+    unsigned int num;
+    int count = 0;
+
+    if (n < 0)
+    {
+        count += _putchar('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    if (num / 10)
+        count += print_num(num / 10);
+
+    count += _putchar((num % 10) + '0');
+
+    return (count);
+}
