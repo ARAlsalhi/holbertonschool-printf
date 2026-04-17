dced00557a5156d228b613d651cac698da3c14e2
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-17 15:22:17 +0300

    Add %c, %s, %% specifiers and NULL check, and unknown specifier handling to printf

--- printf.c
+++ printf.c
@@ -1,1 +1,58 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: string containing the format
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += pString(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(*format);
+					break;
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
