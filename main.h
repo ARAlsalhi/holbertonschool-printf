f2705564a03ee3efac133dc85be41447be1aa539
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-04-18 00:31:55 +0300

    update main.h file we fix the betty order

--- main.h
+++ main.h
@@ -5,28 +5,28 @@
 #include <unistd.h>
 
 /**
- 
-*struct buffer_s - stores buffered output data
-*@output: array used to store output characters
-*@index: current position in the output buffer*/
+ * struct buffer_s - stores buffered output data
+ * @output: array used to store output characters
+ * @index: current position in the output buffer
+ */
 struct buffer_s
 {
-    char output[1024];
-    int index;
+	char output[1024];
+	int index;
 };
 
 typedef struct buffer_s buffer_t;
 
-int _printf(const char format, ...);
-int _putchar_buffer(buffer_tbuffer, char c);
-void flush_buffer(buffer_t buffer);
-int pString(charstring, buffer_t buffer);
-int print_num(int n, buffer_tbuffer);
-int print_unsigned(unsigned int n, buffer_t buffer);
-int print_binary(unsigned int n, buffer_tbuffer);
-int print_octal(unsigned int n, buffer_t buffer);
-int print_hex_lower(unsigned int n, buffer_tbuffer);
-int print_hex_upper(unsigned int n, buffer_t buffer);
-int print_string_custom(charstring, buffer_t *buffer);
+int _printf(const char *format, ...);
+int _putchar_buffer(buffer_t *buffer, char c);
+void flush_buffer(buffer_t *buffer);
+int pString(char *string, buffer_t *buffer);
+int print_num(int n, buffer_t *buffer);
+int print_unsigned(unsigned int n, buffer_t *buffer);
+int print_binary(unsigned int n, buffer_t *buffer);
+int print_octal(unsigned int n, buffer_t *buffer);
+int print_hex_lower(unsigned int n, buffer_t *buffer);
+int print_hex_upper(unsigned int n, buffer_t *buffer);
+int print_string_custom(char *string, buffer_t *buffer);
 
 #endif
