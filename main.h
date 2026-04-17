3d966d6e6a4a49f2078fd9566cc90e2488ec4de0
Lama <lama72316@gmail.com>
2026-04-17 14:58:55 +0000

    update main.h file and replace global buffer with local buffer struct

--- main.h
+++ main.h
@@ -4,15 +4,28 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct buffer_s - stores buffered output data
+ * @output: array used to store output characters
+ * @index: current position in the output buffer
+ */
+struct buffer_s
+{
+    char output[1024];
+    int index;
+};
+
+typedef struct buffer_s buffer_t;
+
 int _printf(const char *format, ...);
-int _putchar(char c);
-int pString(char *string);
-int print_num(int n);
-int print_binary(unsigned int n);
-int print_unsigned(unsigned int n);
-int print_octal(unsigned int n);
-int print_hex_lower(unsigned int n);
-int print_hex_upper(unsigned int n);
-int _putchar_buffer(char c);
-void flush_buffer(void);
+int _putchar_buffer(buffer_t *buffer, char c);
+void flush_buffer(buffer_t *buffer);
+int pString(char *string, buffer_t *buffer);
+int print_num(int n, buffer_t *buffer);
+int print_unsigned(unsigned int n, buffer_t *buffer);
+int print_binary(unsigned int n, buffer_t *buffer);
+int print_octal(unsigned int n, buffer_t *buffer);
+int print_hex_lower(unsigned int n, buffer_t *buffer);
+int print_hex_upper(unsigned int n, buffer_t *buffer);
+
 #endif
